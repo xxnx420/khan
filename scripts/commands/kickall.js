5dac742cf8cb50c0f60354ae4fb1a4c946a75f84
khan43215 <165830398+khan43215@users.noreply.github.com>
2024-04-04 05:10:43 +0600

    Update kickall.js

--- scripts/commands/kickall.js
+++ scripts/commands/kickall.js
@@ -1,24 +1,33 @@
 module.exports.config = {
-  name: "kickall",
-  version: "1.0.0",
-  permission: 2,
-  credits: "Khánh Milo",
-  description: "Kick out all the member inside of the group.",
-  category: "group",
-  usages: "[]",
-  cooldowns: 3,
-  prefix: true
+    name: "gckick",
+    version: "1.0.0",
+    permssion: 2,
+    credits: "ATF-TEAM",
+    description: "Remove all group members.",
+  prefix: true,
+    category: "box chat",
+    usages: "",
+    cooldowns: 5
 };
-
-module.exports.run = async function({ api, event, args }) {
-  var threadInfo = await api.getThreadInfo(event.threadID)
-  var id = threadInfo.participantIDs
-  const user = args.join(" ")
+module.exports.run = async function({ api, event, getText,args }) {
+  const { participantIDs } = await api.getThreadInfo(event.threadID)
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   };
-  for (let user of id) {
-    await delay(5000)
-    api.removeUserFromGroup(user, event.threadID, user);
-  }
-};
+  const botID = api.getCurrentUserID();
+  const listUserID = participantIDs.filter(ID => ID != botID);
+  return api.getThreadInfo(event.threadID, (err, info) => {
+    if (err) return api.sendMessage("» An error occurred.", event.threadID);
+    if (!info.adminIDs.some(item => item.id == api.getCurrentUserID()))
+      return api.sendMessage(`» Need group admin rights.\nPlease add and try again.`, event.threadID, event.messageID);
+    if (info.adminIDs.some(item => item.id == event.senderID)) {
+      setTimeout(function() { api.removeUserFromGroup(botID, event.threadID) }, 300000);
+      return api.sendMessage(`» Start deleting all members. Bye everyone.`, event.threadID, async (error, info) => {
+        for (let id in listUserID) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          api.removeUserFromGroup(listUserID[id], event.threadID)
+        }
+      })
+    } else return api.sendMessage('» Only group admins can use this command.', event.threadID, event.messageID);
+  })
+                           }
