d21e98b65f1649413cc842866450f01346de36ab
khan43215 <165830398+khan43215@users.noreply.github.com>
2024-04-04 07:09:23 +0600

    Create ramadan.js

---
+++ scripts/commands/ramadan.js
@@ -0,0 +1,67 @@
+module.exports.config = {
+  name: "ramadan",
+  version: "0.0.1",
+  permission: 0,
+  prefix: true,
+  credits: "Nayan",
+  description: "Ramadan",
+  category: "user",
+  usages: "",
+    cooldowns: 5,
+};
+
+
+module.exports.run = async function({
+  event: e,
+  api: a,
+  args: n
+}) {
+  if (!n[0]) return a.sendMessage("====「 🆁🅰🅼🅰🅳🅰🅽 」====\n━━━━━━━━━━━━━\n𝟙. 𝐒𝐞𝐡𝐫𝐢 𝐓𝐢𝐦𝐞\n𝟚. 𝐈𝐟𝐭𝐚𝐫 𝐓𝐢𝐦𝐞\n𝟛. 𝐈𝐟𝐭𝐚𝐫 𝐃𝐮𝐚\n𝟜. 𝐑𝐮𝐣𝐚𝐫 𝐍𝐢𝐲𝐨𝐭\n𝟝. 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐚𝐫 𝐊𝐚𝐫𝐨𝐧\n𝟞. 𝐉𝐞𝐠𝐮𝐥𝐚 𝐊𝐨𝐫𝐥𝐞 𝐑𝐮𝐣𝐚 𝐕𝐚𝐧𝐠𝐞 𝐧𝐚\n\n𝐑𝐞𝐩𝐥𝐲 𝐓𝐡𝐢𝐬 𝐌𝐬𝐠 𝐀𝐧𝐝 𝐒𝐞𝐥𝐞𝐜𝐭 𝐍𝐮𝐦𝐛𝐞𝐫", e.threadID, ((a, n) => {
+    global.client.handleReply.push({
+      name: this.config.name,
+      messageID: n.messageID,
+      author: e.senderID,
+      type: "create"
+    })
+  }), e.messageID)
+}, module.exports.handleReply = async ({
+  api: e,
+  event: a,
+  client: n,
+  handleReply: t,
+  Currencies: s,
+  Users: i,
+  Threads: o
+}) => {
+  var { p, h } = linkanh();
+  const request = require("request");
+  if ("create" === t.type) {
+    const n = (await p.get(h)).data.data;
+    const time = (await p.get(h)).data.times.time;
+    const msg = (await p.get(h)).data.msg;
+
+
+    return e.sendMessage({
+      body: `${msg}: ${time}`
+
+    }, a.threadID, a.messageID)
+  }
+
+    function linkanh() {
+        const p = require("axios");
+        const n = "http://nl2-3.deploy.sbs:2011";
+        if ("1" == a.body)
+            var h = `${n}/nayan/sehri`;
+        else if ("2" == a.body)
+         var   h = `${n}/nayan/iftar`;
+      else if ("3" == a.body)
+         var   h = `${n}/nayan/iftardua`;
+      else if ("4" == a.body)
+         var   h = `${n}/nayan/rujarniyot`;
+      else if ("5" == a.body)
+         var   h = `${n}/nayan/rujavk`;
+      else if ("6" == a.body)
+         var   h = `${n}/nayan/rujavkn`;
+        return { p, h };
+    }
+};
