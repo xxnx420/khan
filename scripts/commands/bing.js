0da08f3bad85fb2dc08119cbba1b1ef7512c8847
khan43215 <165830398+khan43215@users.noreply.github.com>
2024-04-09 21:47:57 +0600

    Create bing.js

---
+++ scripts/commands/bing.js
@@ -0,0 +1,60 @@
+ module.exports = {
+  config: {
+    name: "bing",
+    version: "1.0.0",
+    permission: 0,
+    credits: "Nayan",
+    description: "",
+    prefix: 'awto',
+    category: "auto prefix",
+    usages: "bing prompt",
+    cooldowns: 10,
+},
+
+   languages: {
+   "vi": {},
+       "en": {
+           "missing": 'use : /bing cat'
+       }
+   },
+
+start: async function({ nayan, events, args, lang}) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const request = require("request");
+    const prompt = args.join(" ");
+    const key = this.config.credits;
+    const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api2
+    if(!prompt) return nayan.reply(lang('missing'), events.threadID, events.messageID)
+
+  const rndm = ['1FTU5CzXumVMAVcROKAMr9ipQnSRuspdYNABwaOTB-hSRFHUf2hOl1KRFgtJ4QjuIyPhEk0UggP09IblXFmeSuOQ1CCo2KXdMOfVqlZHdApbAoCUu-9CO8KYs8SpOKMjWB3EP7qc0WnLHD56qwjmmnqjOn7ijIXKXnwSsRyhGL0SokMT_xm5ASBPU28ckTwNTnr4zH9K3kBBml5qgn8YzaMWN0JxLgXQMSlZsVAF4Cio'] // input your cookie hare
+
+  var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+
+
+    const res = await axios.get(`${n}/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+  
+  console.log(res.data)
+    const data = res.data.result;
+  const numberSearch = data.length
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+      let path = __dirname + `/cache/${num+=1}.jpg`;
+      let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+      imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
+    }
+
+    
+    nayan.reply({
+        attachment: imgData,
+        body: "🔍Bing Search Result🔍\n\n📝Prompt: " + prompt + "\n\n#️⃣Number of Images: " + numberSearch
+    }, events.threadID, events.messageID)
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`)
+    }
+}
+ }
